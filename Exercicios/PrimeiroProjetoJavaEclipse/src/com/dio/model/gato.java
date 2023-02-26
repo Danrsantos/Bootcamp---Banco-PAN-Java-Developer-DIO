120c659f4b65a971f417c0b6c0d2932d0add9b12
Danilo <danilo.r.s.santos@gmail.com>
2023-02-25 23:57:57 -0300

    Dominando IDEs Java

---
+++ Exercicios/PrimeiroProjetoJavaEclipse/src/com/dio/model/gato.java
@@ -0,0 +1,15 @@
+package com.dio.model;
+
+import java.util.Objects;
+
+public class gato {
+	private String nome;
+	private String cor;
+	private Integer idade;
+
+	public gato() {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+}
---
+++ Exercicios/PrimeiroProjetoJavaEclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Danilo
+ *
+ */
+module PrimeiroProjetoJavaEclipse {
+}
